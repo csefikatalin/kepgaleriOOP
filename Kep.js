2ebad8237e64b3e278ce02f8092f53a1063ed700
csefikatalin <csefi.katalin@gmail.com>
2025-03-12 17:31:34 +0100

    insertAdjacentHTML

--- Kep.js
+++ Kep.js
@@ -8,21 +8,33 @@ megjelenit
 */
 export default class Kep {
 
-  constructor(kep, cim,  szuloElem) {
- 
+  constructor(kep, cim, szuloElem) {
+
     this.kep = kep;
     this.cim = cim;
 
     this.szuloElem = szuloElem;
-    this.megjelenit();
-    
+    this.megjelenit()
+    //this.kepElem = document.querySelector(".kep:last-child")
+    this.kepElem = this.szuloElem.lastElementChild
+    console.log(this.kepElem)
+    this.kepElem.addEventListener("click", function (event) {
+      console.log(event.target.closest("div"))
+      console.log(this)
+    })
+
+
   }
-  megjelenit(){
-    let html=`
-          <div class="kep">
+  megjelenit() {
+    let html = `
+          <div class="kep" >
                 <img src="${this.kep}" alt="${this.cim}">               
             </div>
     `
-    this.szuloElem.innerHTML+=html
+
+    //this.szuloElem.innerHTML+=html;
+    this.szuloElem.insertAdjacentHTML("beforeend", html)
   }
+
+
 }
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import NagyKep from "./NagyKep.js";
 const GALERIAELEM = document.querySelectorAll(".galeria")[0];
 const NAGYKEPELEM = document.querySelectorAll(".kiemelt")[0];
 
-new NagyKep(NAGYKEPELEM)
+//new NagyKep(NAGYKEPELEM)
 
 /* e helyett kellene egy Kepek osztály, 
 ami egy ciklussal végigmegy a listán */
